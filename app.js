7123637ee00a7d2b6551b3af2c6abca4e52b7855
s.akiyoshi <s.akiyoshi.cel@gmail.com>
2020-04-04 17:33:31 +0900

    トリポナッチへ修正

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const memo = new Map();
 memo.set(0,0);
 memo.set(1,0);
 memo.set(2,1);
+
 function fib(n){
     if(memo.has(n)) return memo.get(n);
     const value = fib(n-1) + fib(n-2) + fib(n-3);
